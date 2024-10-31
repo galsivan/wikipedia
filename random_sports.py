abfbe97173455a0bb48fbe272c8c9f1f66deef52
Gal Sivan <gsivan@appnexus.com>
2024-10-31 18:06:43 -0400

    Changed script

--- random_sports.py
+++ random_sports.py
@@ -1,20 +1,25 @@
 import requests
 import random
 
-def get_random_sports_article():
+def fetch_sports_articles():
     url = "https://en.wikipedia.org/w/api.php"
     params = {
         "action": "query",
         "list": "categorymembers",
         "cmtitle": "Category:Sports",
         "cmlimit": "500",
+        "cmnamespace": "0",
         "format": "json"
     }
 
     response = requests.get(url, params=params)
     data = response.json()
 
-    articles = data.get("query", {}).get("categorymembers", [])
+    articles = [item for item in data.get("query", {}).get("categorymembers", []) if item["ns"] == 0]
+    return articles
+
+def get_random_sports_article():
+    articles = fetch_sports_articles()
     if not articles:
         print("No articles found in the Sports category.")
         return
